030ac018a0e9f8656b956b237cbe0a2626547f3c
Tom <tom@Toms-MacBook-Air.local>
2019-04-08 23:19:22 +0100

    Updating App.js and Prismic configuration

--- src/App.js
+++ src/App.js
@@ -3,22 +3,26 @@ import {
   BrowserRouter as Router,
   Route,
   Switch,
-  Redirect,
 } from 'react-router-dom';
-import NotFound from './NotFound';
-import './App.css';
-import Document from './Document';
-
+import Preview from './Preview';
+import NotFound from './components/NotFound';
+import HomePage from './components/HomePage';
+import Page from './components/Page';
+import Pages from './components/Pages'
+import Document from './components/Document'
+import Documents from './components/Documents'
 
 const App = (props) => (
   <Router>
     <Switch>
-      <Redirect exact from="/" to="/document/document1"/>
-      <Route exact path="/document/:uid" render={routeProps => <Document {...routeProps} prismicCtx={props.prismicCtx} />} />
+      <Route exact path="/" render={routeProps => <HomePage {...routeProps} prismicCtx={props.prismicCtx} />} />
+      <Route exact path="/preview" render={routeProps => <Preview {...routeProps} prismicCtx={props.prismicCtx} />} />
+      <Route exact path="/pages/:uid" render={routeProps => <Page {...routeProps} prismicCtx={props.prismicCtx} />} />
+      <Route exact path="/pages" render={routeProps => <Pages {...routeProps} prismicCtx={props.prismicCtx} />} />
+      <Route exact path="/documents/:uid" render={routeProps => <Document {...routeProps} prismicCtx={props.prismicCtx} />} />
+      <Route exact path="/documents" render={routeProps => <Documents {...routeProps} prismicCtx={props.prismicCtx} />} />
       <Route component={NotFound} />
     </Switch>
-
   </Router>
 );
-
 export default App;
