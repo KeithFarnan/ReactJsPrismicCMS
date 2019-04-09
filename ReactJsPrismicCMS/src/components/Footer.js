b38893c7dab11634831019b84a8fbf021c3db2c1
Tom <tom@Toms-MacBook-Air.local>
2019-04-09 13:12:33 +0100

    added href to github

--- ReactJsPrismicCMS/src/components/Footer.js
+++ ReactJsPrismicCMS/src/components/Footer.js
@@ -5,7 +5,7 @@ export default class Footer extends React.Component {
     return (
       <footer>
         <p>
-          GitHub link ;<a href="https://prismic.io" target="_blank" rel="noopener noreferrer">Github</a>
+          GitHub link ;<a href="https://github.com/KeithFarnan/ReactJsPrismicCMS" target="_blank" rel="noopener noreferrer">Github</a>
           <br/>
           <a href="https://prismic.io" target="_blank" rel="noopener noreferrer">
             <img className="footer-logo" src="/images/logo-prismic.svg" alt="Gray Prismic logo"/>
--- ReactJsPrismicCMS/src/index.js
+++ ReactJsPrismicCMS/src/index.js
@@ -6,6 +6,3 @@ ReactDOM.render(
   <PrismicApp />,
   document.getElementById('root'),
 );
-
-//const PORT = process.env.PORT || 5000;
-//app.listen(PORT);
