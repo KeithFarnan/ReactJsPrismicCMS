902a33cbf71844333451ca78ecded4a9f124ef2e
m <m@localhost.localdomain>
2019-05-31 21:30:34 +0100

    Added project links to the Footer

--- src/Components/Layout/Footer.js
+++ src/Components/Layout/Footer.js
@@ -5,13 +5,13 @@ export default class Footer extends React.Component {
   render() {
     return (
       <footer>
-
-          GitHub link<a href="https://prismic.io" target="_blank" rel="noopener noreferrer">Github</a>
-          <br/>
-          <a href="https://prismic.io" target="_blank" rel="noopener noreferrer">
-            <img className="footer-logo" src="/images/logo-prismic.svg" alt="Gray Prismic logo"/>
-          </a>
-        
+        <a href="https://prismic.io" target="_blank" rel="noopener noreferrer">
+        Link to GitHub repository 
+        </a>
+        <br/>
+        <a href="https://prismic.io" target="_blank" rel="noopener noreferrer">
+          <img className="footer-logo" src="/images/logo-prismic.svg" alt="Gray Prismic logo"/>
+        </a>
       </footer>
     );
   }
