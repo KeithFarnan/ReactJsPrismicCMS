68cc9829dbd6422ed08c9e42eed8024b42986c50
m <m@localhost.localdomain>
2019-05-31 20:58:15 +0100

    Cleaned up  index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import PrismicApp from './PrismicApp';
-
 import * as registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
   <PrismicApp />,
   document.getElementById('root'),
 );
-
 registerServiceWorker.register();
-
-//const PORT = process.env.PORT || 5000;
-//PrismicApp.listen(PORT);
