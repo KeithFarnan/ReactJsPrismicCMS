c3d81b6bd8e566ed0b4cb31faab34a0ec7f61c12
Tom <tom@Toms-MacBook-Air.local>
2019-06-01 10:08:29 +0100

    Added layout components and installed mdbreact

---
+++ src/Components/Layout/Dropdown.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { MDBDropdown, MDBDropdownToggle, MDBDropdownMenu, MDBDropdownItem } from "mdbreact";
+import { BrowserRouter } from 'react-router-dom';
+import { MDBNav, MDBNavItem, MDBNavLink } from "mdbreact";
+import {Link} from 'react-router-dom';
+
+const DropdownPage = () => {
+  return (
+    <MDBDropdown>
+      <MDBDropdownToggle caret color="dark">
+        DOCUMENTS
+      </MDBDropdownToggle>
+
+      <MDBDropdownMenu basic>
+
+        <MDBDropdownItem>
+        <Link to="/home">
+        All Documents
+        </Link>
+        </MDBDropdownItem>
+        <MDBDropdownItem >Another Action</MDBDropdownItem>
+        <MDBDropdownItem divider />
+        <MDBDropdownItem>Separated link</MDBDropdownItem>
+      </MDBDropdownMenu>
+    </MDBDropdown>
+  );
+}
+
+export default DropdownPage;
